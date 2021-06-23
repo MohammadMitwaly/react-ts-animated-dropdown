f8dde6bb4ddf6c69a93f08d4c80a78c2cea6d0ae
Mo. Mitwaly <mhmadmtwale@gmail.com>
2021-06-23 08:02:03 +0300

    Add setup code for transitions, and css

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+import { CSSTransition } from "react-transition-group";
 import DropdownItem from "./DropdownItem";
 import { ReactComponent as ChevronUpIcon } from "../icons/chevron-up-circle-outline.svg";
 import { ReactComponent as SettingsIcon } from "../icons/cog-outline.svg";
@@ -6,13 +8,23 @@ import "../index.css";
 type Props = {};
 
 const DropdownMenu: React.FC<Props> = ({ children }) => {
+  const [activeMenu, setActiveMenu] = useState("main");
   return (
     <div className="dropdown">
-      <DropdownItem>My Journeys</DropdownItem>
-      {children}
-      <DropdownItem leftIcon={<SettingsIcon />} rightIcon={<ChevronUpIcon />}>
-        Settings
-      </DropdownItem>
+      <CSSTransition 
+        in={activeMenu === "main"} 
+        unmountOnExit 
+        timeout={500} 
+        lassNames="menu-primary">
+          <div className="menu"
+          >
+            <DropdownItem>My Journeys</DropdownItem>
+            {children}
+            <DropdownItem leftIcon={<SettingsIcon />} rightIcon={<ChevronUpIcon />}>
+              Settings
+            </DropdownItem>
+          </div>
+      </CSSTransition>
     </div>
   );
 };
