1444c474f09066f1d79f0a3c3d609999831f4b69
Mo. Mitwaly <62458010+MohammadMitwaly@users.noreply.github.com>
2021-06-26 21:07:44 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { ReactComponent as NotificationsIcon } from "./icons/notifications-circl
 import { ReactComponent as DocumentsIcon } from "./icons/albums-outline.svg";
 
 import { ReactComponent as ChevronOpenIcon } from "./icons/chevron-down-circle-outline.svg";
-// import { ReactComponent as ChartIcon } from "./icons/albums-outline.svg";
 import { ReactComponent as AddIcon } from "./icons/add-circle-outline.svg";
 import DropdownMenu from "./components/DropdownMenu";
 
@@ -16,7 +15,6 @@ function App() {
       <NavItem icon={<DocumentsIcon />} />
 
       <NavItem icon={<ChevronOpenIcon />}>
-        {/* This is where the dropdown will be */}
         <DropdownMenu></DropdownMenu>
       </NavItem>
     </Navbar>
